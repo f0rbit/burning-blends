d764122bc4a37d0d0665112bbe94feff6e579a6d
f0rbit <dev@forbit.dev>
2024-05-01 21:29:19 +0930

    rehype parsing

--- website/src/lib/posts.ts
+++ website/src/lib/posts.ts
@@ -1,14 +1,24 @@
 import path from "path"
 import { readdir } from 'node:fs/promises';
 import { readFile } from 'node:fs/promises';
+import remark_parse from "remark-parse"
 import remark_frontmatter from 'remark-frontmatter'
 import remark_parse_frontmatter from 'remark-parse-frontmatter'
-import { remark } from 'remark';
+import { unified } from 'unified';
+import rehype_format from 'rehype-format';
+import rehype_stringify from 'rehype-stringify';
+import remark_rehype from 'remark-rehype';
+import rehype_document from 'rehype-document';
 
 
-const processor = remark()
+const processor = unified()
+	.use(remark_parse)
 	.use(remark_frontmatter)
 	.use(remark_parse_frontmatter)
+	.use(remark_rehype)
+	.use(rehype_document)
+	.use(rehype_format)
+	.use(rehype_stringify)
 
 const post_groups = ["cafe", "music"];
 
